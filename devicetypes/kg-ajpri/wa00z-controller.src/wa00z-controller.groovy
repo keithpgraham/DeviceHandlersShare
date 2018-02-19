84a2c4ca027d338a199237f36f9a3473628bccb9
Keith <keithpgraham@comcast.net>
2018-02-19 09:58:16 -0800

    Used WA00Z Device Handler as baseline.

---
+++ devicetypes/kg-ajpri/wa00z-controller.src/wa00z-controller.groovy
@@ -0,0 +1,128 @@
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "WA00Z Controller", namespace: "KG-AJPri", author: "Keith Graham & Austin Pritchett") {
+		capability "Actuator"
+		capability "Switch"
+		capability "Switch Level"
+		capability "Refresh"
+		capability "Sensor"
+        capability "Button"
+        capability "Holdable Button"
+        capability "Battery"
+
+		fingerprint deviceId: "0x014f", inClusters: "0x5e,0x86,0x72,0x5b,0x85,0x9b,0x73,0x70,0x80,0x84,0x5a,0x7a", outClusters: "0x5b,0x20"
+		
+	}
+
+tiles {
+		standardTile("button", "device.button", width: 2, height: 2) {
+			state "default", label: "", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
+		}
+        valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat", width: 1, height: 1) {
+			state "battery", label:'${currentValue}% battery', unit:""
+		}
+        main "button"
+		details(["button", "battery"])
+	}
+    
+    preferences {
+        input name: "reverse", type: "bool", title: "Reverse Buttons", description: "Reverse the top and bottom buttons", required: true
+    }
+}
+
+// parse events into attributes
+def parse(String description) {
+	//log.debug "Parsing '${description}'"
+    
+    def results = []
+	if (description.startsWith("Err")) {
+	    results = createEvent(descriptionText:description, displayed:true)
+	} else {
+		def cmd = zwave.parse(description)
+
+		if(cmd) results += zwaveEvent(cmd)
+		if(!results) results = [ descriptionText: cmd, displayed: true ]
+	}
+	return results
+
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.centralscenev1.CentralSceneNotification cmd) {
+    Integer button   
+    if(reverse == true){
+    	if(cmd.sceneNumber == 1){
+        	button = 2
+        }else{
+        	button = 1
+        }
+        log.debug button
+    }else{
+    	button = (cmd.sceneNumber) as Integer
+    }
+    	
+
+    if(cmd.keyAttributes == 0){
+		createEvent(name: "button", value: "pushed", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was pushed", isStateChange: true)
+    }else if(cmd.keyAttributes == 1){
+		createEvent(name: "button", value: "held", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was released", isStateChange: true)
+    }else if(cmd.keyAttributes == 2){
+		createEvent(name: "button", value: "held", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was held", isStateChange: true)
+    }      
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.wakeupv2.WakeUpNotification cmd) {
+	def result = [createEvent(descriptionText: "${device.displayName} woke up", isStateChange: true)]
+
+	 //Only ask for battery if we haven't had a BatteryReport in a while
+	if (!state.lastbatt || (new Date().time) - state.lastbatt > 24*60*60*1000) {
+		result << response(zwave.batteryV1.batteryGet())
+		result << response("delay 1200")  // leave time for device to respond to batteryGet
+	}
+    
+	result << response(zwave.wakeUpV1.wakeUpNoMoreInformation())
+    result
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {    
+    def result = []
+	def map = [ name: "battery", unit: "%" ]
+	if (cmd.batteryLevel == 0xFF) {
+		map.value = 1
+		map.descriptionText = "${device.displayName} battery is low"
+		map.isStateChange = true
+	} else {
+		map.value = cmd.batteryLevel
+	}
+	state.lastbatt = now()
+	result << createEvent(map)
+
+	result
+}
+
+def configure() {
+	
+}
+
+def installed() {
+	initialize()
+}
+
+def updated() {
+	initialize()
+}
+
+def initialize() {
+	sendEvent(name: "numberOfButtons", value: 2)
+}
